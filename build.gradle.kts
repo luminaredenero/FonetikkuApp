1c986423a6905126762661343793a8b9ba4a320d
Luminare De Nero <luminaredenero@gmail.com>
2025-07-28 01:58:16 +0700

    Fix build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+// TAMBAHKAN BLOK INI UNTUK MEMBERITAHU LOKASI DOWNLOAD LIBRARY
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
